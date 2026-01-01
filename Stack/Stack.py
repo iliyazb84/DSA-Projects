c4081960fbe9a44d247450971027e93b6c2e68ee
Amir ali bairami <yarahmadiamirali1@gmail.com>
2026-01-01 10:48:26 +0330

    Stack Structure has added.

---
+++ Stack/Stack.py
@@ -0,0 +1,27 @@
+class Stack:
+    def __init__(self,Max_Store):
+        self._S =[None for i in range(Max_Store)]
+        self.pointer= 0
+        self.Max= Max_Store
+    def push(self,num):
+        if self.pointer == self.Max:
+            raise Exception ("The Stack is full")
+        else:
+            self._S[self.pointer] = num
+            self.pointer+= 1
+            print(f"{num} has benn stored successfully")
+
+    def pop(self):
+        if self.IsEmpty:
+            raise Exception ("The Stack is Empty")
+        else:
+            self.pointer -=1
+            deleted = self._S[self.pointer]
+            print(f"{deleted} has been deleted successfully")
+            return deleted
+    def peek(self):
+        return self._S[self.pointer-1]    
+    def IsEmpty(self):
+        if self.pointer == 0:
+            return True
+        return False
