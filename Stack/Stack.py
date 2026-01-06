59abc48d4e0e0fed971c723a01acf9211c5d5dc1
Notro <iliyazanboori84@gmail.com>
2026-01-06 17:34:54 +0330

    Minor Change - Typos & Small Issues & etc...

--- Stack/Stack.py
+++ Stack/Stack.py
@@ -3,13 +3,14 @@ class Stack:
         self._S =[None for i in range(Max_Store)]
         self.pointer= 0
         self.Max= Max_Store
+
     def push(self,num):
         if self.pointer == self.Max:
-            raise Exception ("The Stack is full")
+            raise Exception ("The Stack is Full!")
         else:
             self._S[self.pointer] = num
             self.pointer+= 1
-            print(f"{num} has benn stored successfully")
+            print(f"{num} Has Been Stored Successfully")
 
     def pop(self):
         if self.IsEmpty():
@@ -17,10 +18,12 @@ class Stack:
         else:
             self.pointer -=1
             deleted = self._S[self.pointer]
-            print(f"{deleted} has been deleted successfully")
+            print(f"{deleted} Has Been Deleted Successfully")
             return deleted
+
     def peek(self):
         return self._S[self.pointer-1]    
+        
     def IsEmpty(self):
         if self.pointer == 0:
             return True
